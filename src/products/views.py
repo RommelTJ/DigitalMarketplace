3e95ddcea0ed18fc4e5627bd926dd5570e3b72b4
Rommel Rico <rommeltj@gmail.com>
2016-05-18 21:05:31 -0700

    Dynamic URL patterns

--- src/products/views.py
+++ src/products/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from models import Product
 
 # Create your views here.
-def detail_view(request):
-    #1 item
-    print(request)
+def detail_view(request, object_id=None):
+    product = get_object_or_404(Product, id=object_id)
     template = "detail_view.html"
-    context = {}
+    context = {
+        "object": product,
+    }
     return render(request, template, context)
-
+    
 def list_view(request):
     #list of items
-    print(request)
+    print(request.user)
+    query_set = Product.objects.all()
     template = "list_view.html"
-    context = {}
+    context = {
+        "query_set": query_set
+    }
     return render(request, template, context)
