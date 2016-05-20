c5697be657206012f0415889769f89287beebe3e
Rommel Rico <rommeltj@gmail.com>
2016-05-19 21:31:12 -0700

    Forms and Form Validation

--- src/digitalmarketplace/urls.py
+++ src/digitalmarketplace/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^detail/(?P<object_id>\d+)/$', products.views.detail_view, name='detail_view'),
     url(r'^detail/(?P<slug>[\w-]+)/$', products.views.detail_slug_view, name='detail_slug_view'),
     url(r'^list/$', products.views.list_view, name='list_view'),
+    url(r'^create/$', products.views.create_view, name='create_view'),
 ]
--- src/products/models.py
+++ src/products/models.py
@@ -1,15 +1,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Product(models.Model):
     title = models.CharField(max_length=30)
-    slug = models.SlugField(default='slug-field') # unique=True
+    slug = models.SlugField(blank=True, unique=True)
     description = models.TextField(default="")
     price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99)
     sale_price = models.DecimalField(max_digits=100, decimal_places=2, default=6.99, null=True, blank=True)
 
     def __unicode__(self):
         return self.title
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
+
+# def product_post_save_receiver(sender, instance, *args, **kwargs):
+#     if instance.slug != slugify(instance.title):
+#         instance.slug = slugify(instance.title)
+#         instance.save()
+#
+# post_save.connect(product_post_save_receiver, sender=Product)
