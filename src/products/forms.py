af9ef34e8e8a5f9521718cf1f09799f216784644
Rommel Rico <rommeltj@gmail.com>
2016-05-20 20:37:08 -0700

    More work on the forms

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from models import Product
 
 PUBLISH_CHOICES = (
     ('', ''),
@@ -7,10 +8,62 @@ PUBLISH_CHOICES = (
 )
 
 class ProductAddForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(label="Your Title", widget=forms.TextInput(
+        attrs={
+            "class": "custom-class",
+            "placeholder": "Title",
+        }))
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={
+            "class": "my-custom-class",
+            "placeholder": "Description",
+            "some-attribute": "this",
+        }
+    ))
     price = forms.DecimalField()
     publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price <= 1.00:
+            raise forms.ValidationError("Price must be greater than 1")
+        elif price >= 99.99:
+            raise forms.ValidationError("Price must be smaller than 99.99")
+        else:
+            return price
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) > 3:
+            return title
+        else:
+            raise forms.ValidationError("Title must be greater than 3 characters long.")
+
+class ProductModelForm(forms.ModelForm):
+    publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
+    # description = forms.CharField(widget=forms.Textarea(
+    #     attrs={
+    #         "class": "my-custom-class",
+    #         "placeholder": "Description",
+    #         "some-attribute": "this",
+    #     }
+    # ))
+
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "description",
+            "price",
+        ]
+        widgets = {
+            "description": forms.Textarea(attrs={
+                "placeholder": "New Placeholder",
+            }),
+            "title": forms.TextInput(attrs={
+                "placeholder": "New Title Placeholder",
+            })
+        }
 
     def clean_price(self):
         price = self.cleaned_data.get("price")
