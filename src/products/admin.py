f5a46cdffcb489b94be297abc57c0a2921e33399
Rommel Rico <rommeltj@gmail.com>
2016-05-26 15:02:23 -0700

    Tabular Inlines

--- src/products/admin.py
+++ src/products/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product, MyProducts, Thumbnail
 
+class ThumbnailInline(admin.TabularInline):
+    extra = 1
+    model = Thumbnail
+
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [ThumbnailInline]
     list_display = ["__unicode__", "description", "price", "sale_price"]
     list_filter = ["price", "sale_price"]
     search_fields = ["title", "description"]
@@ -12,7 +17,5 @@ class ProductAdmin(admin.ModelAdmin):
         model = Product
 
 admin.site.register(Product, ProductAdmin)
-
-admin.site.register(Thumbnail)
 
 admin.site.register(MyProducts)
