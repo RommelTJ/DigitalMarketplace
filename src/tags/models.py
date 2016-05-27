5d22672c804a20ffaf20157d8726be16ff25554c
Rommel Rico <rommeltj@gmail.com>
2016-05-27 10:55:18 -0700

    Model Managers and QuerySet

--- src/tags/models.py
+++ src/tags/models.py
@@ -8,11 +8,24 @@ from django.utils.text import slugify
 from products.models import Product
 
 # Create your models here.
+
+class TagQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+class TagManager(models.Manager):
+    def get_queryset(self):
+        return TagQuerySet(self.model, using=self._db)
+
+    def all(self, *args, **kwargs):
+        return super(TagManager, self).all(*args, **kwargs).active()
+
 class Tag(models.Model):
     title = models.CharField(max_length=120, unique=True)
     slug = models.SlugField(unique=True)
     products = models.ManyToManyField(Product, blank=True)
     active = models.BooleanField(default=True)
+    objects = TagManager()
 
     def __unicode__(self):
         return str(self.title)
--- src/tags/views.py
+++ src/tags/views.py
@@ -14,5 +14,5 @@ class TagDetailView(DetailView):
 class TagListView(ListView):
     model = Tag
 
-    def get_queryset(self):
-        return Tag.objects.filter(active=True)
+    # def get_queryset(self):
+    #     return Tag.objects.filter(active=True)
