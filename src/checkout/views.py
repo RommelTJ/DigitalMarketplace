3f9e0dfbe7f075d18e021fa9b89e38979de65e7c
Rommel Rico <rommeltj@gmail.com>
2016-05-30 17:46:44 -0700

    Purchase Confirmation with AJAX

--- src/checkout/views.py
+++ src/checkout/views.py
@@ -3,7 +3,33 @@ from django.http import HttpResponse, JsonResponse, Http404
 from django.views.generic import View
 from django.shortcuts import render
 
+from products.models import Product, MyProducts
+
 # Create your views here.
+
+class CheckoutAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        print request.POST
+        if request.is_ajax():
+            if not request.user.is_authenticated():
+                return JsonResponse({}, status=403)
+            user = request.user
+            product_id = request.POST.get("product_id")
+            exists = Product.objects.filter(id=product_id).exists()
+            if not exists:
+                return JsonResponse({}, status=404)
+            try:
+                product_obj = Product.object.get(id=product_id)
+            except:
+                product_obj = Product.objects.filter(id=product_id).first()
+            my_products = MyProducts.objects.get_or_create(user=request.user)[0]
+            my_products.products.add(product_obj)
+            data = {
+                "works": True,
+                "time": datetime.datetime.now(),
+            }
+            return JsonResponse(data)
+        raise Http404
 
 class CheckoutTestView(View):
     def post(self, request, *args, **kwargs):
