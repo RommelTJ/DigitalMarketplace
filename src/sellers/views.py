e48d52bb2292a3012d62d2f4b0307812c63b7df5
Rommel Rico <rommeltj@gmail.com>
2016-06-02 20:23:16 -0700

    New Seller Form

--- src/sellers/views.py
+++ src/sellers/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from digitalmarketplace.mixins import LoginRequiredMixin
+from .forms import NewSellerForm
+
 # Create your views here.
 
-class SellerDashboard(View):
+class SellerDashboard(LoginRequiredMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        form = NewSellerForm(request.POST)
+        if form.is_valid():
+            print "Make user apply model"
+        return render(request, "sellers/dashboard.html", {"form":form})
 
     def get(self, request, *args, **kwargs):
-        return render(request, "sellers/dashboard.html", {})
+        form = NewSellerForm()
+        return render(request, "sellers/dashboard.html", {"form":form})
 
